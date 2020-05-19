5917a371b7727ddcc59ee5fae0cbc1dba6b32869
shiratsu <shun_hiratsuka2002@yahoo.co.jp>
2020-05-20 07:21:18 +0900

    いろいろ集計してみた

--- titanic_analysis.py
+++ titanic_analysis.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-import numpy 
+import numpy
 import pandas as pd
 
 import os
-path=os.getcwd()
+path = os.getcwd()
 print(path)
 
 ##############################
@@ -14,6 +14,33 @@ print(path)
 
 train = pd.read_csv("csv/train.csv")
 print(train.head())
+
+# sibspとParchで集計してみる
+print("test")
+print(train.query('Survived == 1'))
+print("test")
+
+# そもそも兄弟いる人と、子供の数を確認
+print(train.groupby(["SibSp"]).count())
+print(train.groupby(["Parch"]).count())
+
+
+# 以下いろいろ計算してみたけど、多分兄弟や配偶者の乗船、子供の乗船がゼロの方が多いので、あまり意味ないかも
+suv0 = train.query('Survived == 0')
+suv1 = train.query('Survived == 1')
+
+# 乗船してる兄弟の人数で生き残れてるのかどうか確認する
+suv0BySibSp = suv0.groupby(["SibSp"]).count()[
+    ["Survived"]]
+print(suv0BySibSp)
+
+suv1BySibSp = suv1.groupby(["SibSp"]).count()[
+    ["Survived"]]
+print(suv1BySibSp)
+
+
+#
+
 
 test = pd.read_csv('csv/test.csv')
 print(test.head())
